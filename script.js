1987c96fcd16a87c2cb960017fa1c7774311b898
freddyspaghetti <frederik.bjorgensen@gmail.com>
2019-05-27 13:11:43 +0200

    alt virker, bøvl med øverste ikon til venstre

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-window.addEventListener("load", sidenVises);
+/* --------------- BURGER MENU --------------- */
 
-function sidenVises() {
-    console.log("sidenVises");
-
-    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-
-
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
 }
 
-
-function toggleMenu() {
-    console.log("toggleMenu");
-    document.querySelector("#menu").classList.toggle("hidden");
-
-    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-
-    if (erSkjult == true) {
-        document.querySelector("#menuknap").textContent = "☰";
-    } else {
-        document.querySelector("#menuknap").textContent = "X";
-    }
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
 }
